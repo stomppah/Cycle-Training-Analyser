5d0ba352d188ed6bda3c244e3a7ca4345dfa6362
Anthony Woodward <a.woodward6237@student.leedsmet.ac.uk>
2014-02-21 17:09:51 +0000

    Added file parsing, all data get's passed into a List<string[]> object. (Needs plumbing in)

--- Analyser/Analyser/Parser.cs
+++ Analyser/Analyser/Parser.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -8,9 +10,37 @@ namespace Analyser
     internal class Parser
     {
         internal List<string[]> hrmData = new List<string[]>();
+        internal int guessMaxColumns = 10;
 
         public Parser() { }
 
+        public List<string[]> ReadDataFromStream(Stream stream)
+        {
+            // Read stream line by line
+            string line;
+            if (stream != null) {
+                StreamReader file = null;
+               try {
+                  file = new StreamReader(stream);
+                  while ((line = file.ReadLine()) != null) {
+
+                      // split line into columns and stick them in the list here
+                      string[] stats = line.Split(' ');
+                      hrmData.Add(stats);
+
+                      foreach (string stat in stats) 
+                      {  
+                         Debug.WriteLine(stat);
+                      }
+                  }
+               } finally {
+                  if (file != null)
+                     file.Close();
+               }
+            }
+
+            return hrmData;
+        }
 
     }
 }
