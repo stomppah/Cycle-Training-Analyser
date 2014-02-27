4b1d807756c09fda8a340e7e4bff8ba97661d294
Anthony Woodward <a.woodward6237@student.leedsmet.ac.uk>
2014-02-27 14:28:10 +0000

    setting up interval class

--- Analyser/Analyser/HRMDataInterval.cs
+++ Analyser/Analyser/HRMDataInterval.cs
@@ -7,5 +7,16 @@ namespace Analyser
 {
     class HRMDataInterval
     {
+        private int m_bpm, m_speed, m_cadence, m_altitude, m_power, m_powerBalance;
+
+        public HRMDataInterval(int bpm, int speed, int cadence, int altitude, int power, int powerBalance)
+        {
+            m_bpm = bpm;
+            m_speed = speed;
+            m_cadence = cadence;
+            m_altitude = altitude;
+            m_power = power;
+            m_powerBalance = powerBalance;
+        }
     }
 }
