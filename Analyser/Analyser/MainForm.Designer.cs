542f78c141bb4a9d892659deb784c77853b34c13
Anthony Woodward <a.woodward6237@student.leedsmet.ac.uk>
2014-05-08 10:55:25 +0100

    gui changes

--- Analyser/Analyser/MainForm.Designer.cs
+++ Analyser/Analyser/MainForm.Designer.cs
@@ -45,12 +45,30 @@
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
             this.exerciseDataPanel = new System.Windows.Forms.Panel();
+            this.summaryInfoGroup = new System.Windows.Forms.GroupBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.dateLabel = new System.Windows.Forms.Label();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.checkedSmodeList = new System.Windows.Forms.CheckedListBox();
             this.MainMenuStrip.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.tabPage2.SuspendLayout();
             this.exerciseDataPanel.SuspendLayout();
+            this.summaryInfoGroup.SuspendLayout();
             this.SuspendLayout();
             // 
             // MainMenuStrip
@@ -59,7 +77,7 @@
             this.fileToolStripMenuItem});
             this.MainMenuStrip.Location = new System.Drawing.Point(0, 0);
             this.MainMenuStrip.Name = "MainMenuStrip";
-            this.MainMenuStrip.Size = new System.Drawing.Size(1008, 24);
+            this.MainMenuStrip.Size = new System.Drawing.Size(1015, 24);
             this.MainMenuStrip.TabIndex = 0;
             this.MainMenuStrip.Text = "menuStrip1";
             // 
@@ -85,13 +103,15 @@
             // 
             // tabControl1
             // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Location = new System.Drawing.Point(14, 15);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1008, 505);
+            this.tabControl1.Size = new System.Drawing.Size(724, 531);
             this.tabControl1.TabIndex = 5;
             // 
             // tabPage1
@@ -100,7 +120,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1000, 479);
+            this.tabPage1.Size = new System.Drawing.Size(716, 505);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Data View";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -118,7 +138,7 @@
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView1.Location = new System.Drawing.Point(3, 3);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(994, 473);
+            this.dataGridView1.Size = new System.Drawing.Size(710, 499);
             this.dataGridView1.TabIndex = 1;
             // 
             // Column1
@@ -157,7 +177,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1190, 523);
+            this.tabPage2.Size = new System.Drawing.Size(750, 505);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Graph View";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -176,25 +196,232 @@
             this.zedGraphControl1.ScrollMinX = 0D;
             this.zedGraphControl1.ScrollMinY = 0D;
             this.zedGraphControl1.ScrollMinY2 = 0D;
-            this.zedGraphControl1.Size = new System.Drawing.Size(1184, 517);
+            this.zedGraphControl1.Size = new System.Drawing.Size(744, 499);
             this.zedGraphControl1.TabIndex = 0;
             this.zedGraphControl1.Visible = false;
             // 
             // exerciseDataPanel
             // 
+            this.exerciseDataPanel.Controls.Add(this.summaryInfoGroup);
+            this.exerciseDataPanel.Controls.Add(this.label9);
+            this.exerciseDataPanel.Controls.Add(this.label8);
+            this.exerciseDataPanel.Controls.Add(this.label7);
+            this.exerciseDataPanel.Controls.Add(this.label6);
+            this.exerciseDataPanel.Controls.Add(this.label5);
+            this.exerciseDataPanel.Controls.Add(this.label4);
+            this.exerciseDataPanel.Controls.Add(this.label3);
+            this.exerciseDataPanel.Controls.Add(this.label2);
+            this.exerciseDataPanel.Controls.Add(this.label1);
             this.exerciseDataPanel.Controls.Add(this.tabControl1);
             this.exerciseDataPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.exerciseDataPanel.Location = new System.Drawing.Point(0, 24);
             this.exerciseDataPanel.Margin = new System.Windows.Forms.Padding(10);
             this.exerciseDataPanel.Name = "exerciseDataPanel";
-            this.exerciseDataPanel.Size = new System.Drawing.Size(1008, 705);
+            this.exerciseDataPanel.Size = new System.Drawing.Size(1015, 705);
             this.exerciseDataPanel.TabIndex = 6;
+            // 
+            // summaryInfoGroup
+            // 
+            this.summaryInfoGroup.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.summaryInfoGroup.Controls.Add(this.checkedSmodeList);
+            this.summaryInfoGroup.Controls.Add(this.label15);
+            this.summaryInfoGroup.Controls.Add(this.timeLabel);
+            this.summaryInfoGroup.Controls.Add(this.dateLabel);
+            this.summaryInfoGroup.Controls.Add(this.label12);
+            this.summaryInfoGroup.Controls.Add(this.label11);
+            this.summaryInfoGroup.Controls.Add(this.label10);
+            this.summaryInfoGroup.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.summaryInfoGroup.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.summaryInfoGroup.Location = new System.Drawing.Point(744, 15);
+            this.summaryInfoGroup.Name = "summaryInfoGroup";
+            this.summaryInfoGroup.Size = new System.Drawing.Size(261, 531);
+            this.summaryInfoGroup.TabIndex = 15;
+            this.summaryInfoGroup.TabStop = false;
+            this.summaryInfoGroup.Text = "Summary Info";
+            // 
+            // label9
+            // 
+            this.label9.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(737, 652);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(67, 13);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Avg. Altitude";
+            // 
+            // label8
+            // 
+            this.label8.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(737, 608);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(63, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Max. Power";
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(737, 569);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Avg. Power";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(409, 660);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Max. Heart Rate";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(409, 612);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Min. Heart Rate";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(409, 569);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Avg. Heart Rate";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 656);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Max. Speed";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 608);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Avg Speed";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 569);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Total Distance:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(37, 48);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(42, 17);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "Date";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(37, 90);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(43, 17);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "Time";
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(6, 136);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(70, 17);
+            this.label12.TabIndex = 18;
+            this.label12.Text = "Duration";
+            this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // dateLabel
+            // 
+            this.dateLabel.AutoSize = true;
+            this.dateLabel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.dateLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateLabel.Location = new System.Drawing.Point(92, 48);
+            this.dateLabel.Name = "dateLabel";
+            this.dateLabel.Size = new System.Drawing.Size(82, 19);
+            this.dateLabel.TabIndex = 19;
+            this.dateLabel.Text = "dateLabel";
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.timeLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.timeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLabel.Location = new System.Drawing.Point(92, 90);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(63, 19);
+            this.timeLabel.TabIndex = 20;
+            this.timeLabel.Text = "label14";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label15.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(92, 134);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(63, 19);
+            this.label15.TabIndex = 21;
+            this.label15.Text = "label15";
+            // 
+            // checkedSmodeList
+            // 
+            this.checkedSmodeList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkedSmodeList.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkedSmodeList.FormattingEnabled = true;
+            this.checkedSmodeList.Items.AddRange(new object[] {
+            "Speed",
+            "Cadence",
+            "Altitude",
+            "Power",
+            "Heart Rate",
+            "Imperial"});
+            this.checkedSmodeList.Location = new System.Drawing.Point(9, 191);
+            this.checkedSmodeList.Name = "checkedSmodeList";
+            this.checkedSmodeList.Size = new System.Drawing.Size(245, 238);
+            this.checkedSmodeList.TabIndex = 22;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.ClientSize = new System.Drawing.Size(1015, 729);
             this.Controls.Add(this.exerciseDataPanel);
             this.Controls.Add(this.MainMenuStrip);
             this.MinimumSize = new System.Drawing.Size(1024, 768);
@@ -208,6 +435,9 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.tabPage2.ResumeLayout(false);
             this.exerciseDataPanel.ResumeLayout(false);
+            this.exerciseDataPanel.PerformLayout();
+            this.summaryInfoGroup.ResumeLayout(false);
+            this.summaryInfoGroup.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -231,6 +461,23 @@
         private System.Windows.Forms.TabPage tabPage2;
         private ZedGraph.ZedGraphControl zedGraphControl1;
         private System.Windows.Forms.Panel exerciseDataPanel;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox summaryInfoGroup;
+        private System.Windows.Forms.CheckedListBox checkedSmodeList;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label dateLabel;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
     }
 }
 
