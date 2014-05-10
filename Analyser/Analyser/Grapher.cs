0f9b3a496e63991860efe3bc4af84d0973225db5
Anthony Woodward <a.woodward6237@student.leedsmet.ac.uk>
2014-05-10 21:12:16 +0100

    + Refactored code but still needs tidying. + Set extra YAxes to only show if corresponding smode is set.

--- Analyser/Analyser/Grapher.cs
+++ Analyser/Analyser/Grapher.cs
@@ -17,7 +17,7 @@ namespace Analyser
         private static readonly PointPairList SpeedPlotList = new PointPairList();
         private static readonly PointPairList PowerBalancePlotList = new PointPairList();
 
-        private static bool _setupComplete = false;
+        private static bool _setupComplete;
 
         public static void UpdateGraph(ref ZedGraphControl zedGraphControl, ref ExerciseSession exerciseSession)
         {
@@ -74,27 +74,71 @@ namespace Analyser
         private static void SetupGraphAxes()
         {
             // Set the titles and axis labels
-            _myPane.Title.Text = "Demonstration of Multi Y Graph";
+            _myPane.Title.Text = "Cycling Session Data";
             _myPane.XAxis.Title.Text = "Time";
             _myPane.YAxis.Title.Text = "HR [bpm]";
             _myPane.Y2Axis.Title.Text = "Speed [m/s]";
 
-            // Create a second Y Axis, green
+            setupAltitudeAxis();
+
+            setupCadenceAxis();
+
+            // Make the Y axis scale red
+            _myPane.YAxis.Scale.FontSpec.FontColor = Color.Red;
+            _myPane.YAxis.Title.FontSpec.FontColor = Color.Red;
+            // turn off the opposite tics so the Y tics don't show up on the Y2 axis
+            //myPane.YAxis.IsOppositeTic = false;
+            //myPane.YAxis.IsMinorOppositeTic = false;
+            // Don't display the Y zero line
+            //myPane.YAxis.IsZeroLine = false;
+            // Align the Y axis labels so they are flush to the axis
+            _myPane.YAxis.Scale.Align = AlignP.Inside;
+            //myPane.YAxis.Max = 100;
+
+            // Enable the Y2 axis display
+            _myPane.Y2Axis.IsVisible = Extensions.IsFlagSet(_exerciseSession.CurrentSMode, Smode.Speed);
+            // Make the Y2 axis scale blue
+            _myPane.Y2Axis.Scale.FontSpec.FontColor = Color.Blue;
+            _myPane.Y2Axis.Title.FontSpec.FontColor = Color.Blue;
+            // turn off the opposite tics so the Y2 tics don't show up on the Y axis
+            //myPane.Y2Axis.IsOppositeTic = false;
+            //myPane.Y2Axis.IsMinorOppositeTic = false;
+            // Display the Y2 axis grid lines
+            //myPane.Y2Axis.IsShowGrid = true;
+            // Align the Y2 axis labels so they are flush to the axis
+            _myPane.Y2Axis.Scale.Align = AlignP.Inside;
+            //myPane.Y2Axis.Min = 1.5;
+            //myPane.Y2Axis.Max = 3;
+
+            // Fill the axis background with a gradient
+            _myPane.Fill = new Fill(Color.White, Color.LightGoldenrodYellow, 45.0f);
+
+            _setupComplete = true;
+            GenerateExample();
+        }
+
+        private static void setupAltitudeAxis()
+        {
+// Create a second Y Axis, green
             YAxis yAxis3 = new YAxis("Altitude [m]");
+            yAxis3.IsVisible = Extensions.IsFlagSet(_exerciseSession.CurrentSMode, Smode.Altitude);
             _myPane.YAxisList.Add(yAxis3);
             yAxis3.Scale.FontSpec.FontColor = Color.Green;
             yAxis3.Title.FontSpec.FontColor = Color.Green;
             yAxis3.Color = Color.Green;
             // turn off the opposite tics so the Y2 tics don't show up on the Y axis
-            //yAxis3.IsInsideTic = false;
+            yAxis3.IsAxisSegmentVisible = false; // IsInsideTic = false;
             //yAxis3.IsMinorInsideTic = false;
             //yAxis3.IsOppositeTic = false;
             //yAxis3.IsMinorOppositeTic = false;
             // Align the Y2 axis labels so they are flush to the axis
             yAxis3.Scale.Align = AlignP.Inside;
+        }
 
+        private static void setupCadenceAxis()
+        {
             Y2Axis yAxis4 = new Y2Axis("Cadence [rpm]");
-            yAxis4.IsVisible = true;
+            yAxis4.IsVisible = Extensions.IsFlagSet(_exerciseSession.CurrentSMode, Smode.Cadence);
             _myPane.Y2AxisList.Add(yAxis4);
             // turn off the opposite tics so the Y2 tics don't show up on the Y axis
             //yAxis4.IsInsideTic = false;
@@ -105,25 +149,19 @@ namespace Analyser
             yAxis4.Scale.Align = AlignP.Inside;
             yAxis4.Type = AxisType.Log;
             //yAxis4.Min = 100;
-
-            // Fill the axis background with a gradient
-            _myPane.Fill = new Fill(Color.White, Color.LightGoldenrodYellow, 45.0f);
-
-            _setupComplete = true;
-            GenerateExample();
         }
 
         private static void GenerateExample()
         {
             // Generate a red curve with diamond symbols, and "Velocity" in the legend
             var myCurve = _myPane.AddCurve("HR",
-              HrPlotList, Color.Red, SymbolType.HDash);
+              HrPlotList, Color.Red, SymbolType.XCross);
             // Fill the symbols with white
             myCurve.Symbol.Fill = new Fill(Color.White);
 
             // Generate a blue curve with circle symbols, and "Acceleration" in the legend
             myCurve = _myPane.AddCurve("Speed",
-               SpeedPlotList, Color.Blue, SymbolType.HDash);
+               SpeedPlotList, Color.Blue, SymbolType.Default);
             // Fill the symbols with white
             myCurve.Symbol.Fill = new Fill(Color.White);
             // Associate this curve with the Y2 axis
@@ -148,34 +186,7 @@ namespace Analyser
             myCurve.YAxisIndex = 1;
 
             // Show the x axis grid
-            _myPane.XAxis.IsVisible = true; //IsShowGrid = true;
-
-            // Make the Y axis scale red
-            _myPane.YAxis.Scale.FontSpec.FontColor = Color.Red;
-            _myPane.YAxis.Title.FontSpec.FontColor = Color.Red;
-            // turn off the opposite tics so the Y tics don't show up on the Y2 axis
-            //myPane.YAxis.IsOppositeTic = false;
-            //myPane.YAxis.IsMinorOppositeTic = false;
-            // Don't display the Y zero line
-            //myPane.YAxis.IsZeroLine = false;
-            // Align the Y axis labels so they are flush to the axis
-            _myPane.YAxis.Scale.Align = AlignP.Inside;
-            //myPane.YAxis.Max = 100;
-
-            // Enable the Y2 axis display
-            _myPane.Y2Axis.IsVisible = true;
-            // Make the Y2 axis scale blue
-            _myPane.Y2Axis.Scale.FontSpec.FontColor = Color.Blue;
-            _myPane.Y2Axis.Title.FontSpec.FontColor = Color.Blue;
-            // turn off the opposite tics so the Y2 tics don't show up on the Y axis
-            //myPane.Y2Axis.IsOppositeTic = false;
-            //myPane.Y2Axis.IsMinorOppositeTic = false;
-            // Display the Y2 axis grid lines
-            //myPane.Y2Axis.IsShowGrid = true;
-            // Align the Y2 axis labels so they are flush to the axis
-            _myPane.Y2Axis.Scale.Align = AlignP.Inside;
-            //myPane.Y2Axis.Min = 1.5;
-            //myPane.Y2Axis.Max = 3;
+            _myPane.XAxis.IsVisible = true;
 
             //myPane.AxisChange( CreateGraphics() );
         }
