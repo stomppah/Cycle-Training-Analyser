95d15d13a1765012b9689243fc00aa56cc3ba26d
Anthony Woodward <a.woodward6237@student.leedsmet.ac.uk>
2014-05-10 19:12:05 +0100

    pulled interval class out of MainForm

---
+++ Analyser/Analyser/Interval.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analyser
+{
+    class Interval
+    {
+        public double Time { get; set; }
+        public string Bpm { get; set; }
+        public string Speed { get; set; }
+        public string Cadence { get; set; }
+        public string Altitude { get; set; }
+        public string Power { get; set; }
+        public string PowerBalance { get; set; }
+    }
+}
